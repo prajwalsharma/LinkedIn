0cec780018d39007e3407e02669d8c3117da4009
Prajwal Sharma <prjwl4@gmail.com>
2026-01-24 22:23:41 +0530

    feat: create login & register endpoints

--- api/linkedin-api/Controllers/UsersController.cs
+++ api/linkedin-api/Controllers/UsersController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using linkedin_api.Features.Users.LoginUser;
+using Microsoft.AspNetCore.Mvc;
 
 namespace linkedin_api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class UsersController : ControllerBase
     {
-        [HttpPost("login")]
-        public IActionResult Login(string username, string password)
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
         {
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginUserCommand command)
+        {
+            _logger.LogInformation("Login is called - {@command}", command);
             return Ok("Login success!");
         }
 
---
+++ api/linkedin-api/Features/Users/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace linkedin_api.Features.Users.LoginUser
+{
+    public class LoginUserCommand
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public required string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public required string Password { get; set; }
+    }
+}
