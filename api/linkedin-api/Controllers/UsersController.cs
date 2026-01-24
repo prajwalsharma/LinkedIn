ad90041e89a36106e2ded90db7799f88afe80c43
Prajwal Sharma <prjwl4@gmail.com>
2026-01-24 21:53:54 +0530

    feat: add users controller

---
+++ api/linkedin-api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace linkedin_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(string username, string password)
+        {
+            return Ok("Login success!");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(string username, string password)
+        {
+            return Ok("Account Created!");
+        }
+    }
+}
